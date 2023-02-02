4877c71932cd60a6d930d537575c8f1f16e744fc
alessandro.uleon <alessandro.uleon@grupoicts.com.br>
2023-02-02 19:36:44 -0400

    feat: create context, sidebar responsivit

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-    return (
-        <Flex align='center'>
+
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+
+    <Flex align='center'>
+      {showProfileData && (
         <Box mr={4} textAlign='center'>
-        <Text>Alessandro Uleon</Text>
-        <Text color='gray.300' fontSize='small'>alessandro.uleon@gmail.com</Text>
+          <Text>Alessandro Uleon</Text>
+          <Text color='gray.300' fontSize='small'>alessandro.uleon@gmail.com</Text>
         </Box>
-        <Avatar 
-        size='md' 
-        name='Alessadnro Uleon' 
+      )}
+      <Avatar
+        size='md'
+        name='Alessadnro Uleon'
         src="https://xesque.rocketseat.dev/users/avatar/profile-d3073b26-4738-4055-99ad-c0b225bd4781-1674083140242.jpg" />
-      </Flex>
-    );
+
+    </Flex>
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,39 @@
-import { Flex } from "@chakra-ui/react";
+import { useSidebarDrawer } from "@/contexts/SidebarDrawerContext";
+import { Flex, IconButton, useBreakpointValue, Icon } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { Search } from "./Search";
 
+
 export function Header() {
+    const { onOpen } = useSidebarDrawer();
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    });
+
     return (
         <Flex as='header' w='100%' maxWidth={1480} h='20' mx='auto' mt='4' px='6' align='center' >
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant='unstyled'
+                    onClick={onOpen}
+                    mr='2'
+                >
+
+                </IconButton>
+            )}
             <Logo />
-            <Search />
+            {isWideVersion && <Search />}
             <Flex align='center' ml='auto' >
                 <NotificationsNav />
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
             </Flex>
         </Flex>
     );
